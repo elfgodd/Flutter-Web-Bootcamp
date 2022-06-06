1996f470058282fd5224de45a4d034ce418de07f
ElfGod <albertogx@icloud.com>
2022-06-06 09:53:28 -0500

    404 Page

--- 01-basic/lib/router/router.dart
+++ 01-basic/lib/router/router.dart
@@ -1,25 +1,29 @@
 import 'package:basic/ui/views/counter_provider_view.dart';
 import 'package:basic/ui/views/counter_view.dart';
+import 'package:basic/ui/views/view_404.dart';
 import 'package:fluro/fluro.dart';
 
 class Flurorouter {
   static final FluroRouter router = new FluroRouter();
 
   static void configureRoutes() {
-    router.define('/', handler: _counterHandler,
-      transitionType: TransitionType.fadeIn);
-      
-      // This makes the transition take 1 second.
-      // transitionDuration: Duration(milliseconds: 1000),
+    router.define('/',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
 
-      // No transition
-      // transitionType: TransitionType.none);
+    // This makes the transition take 1 second.
+    // transitionDuration: Duration(milliseconds: 1000),
 
-    router.define('/stateful', handler: _counterHandler,
-      transitionType: TransitionType.fadeIn);
+    // No transition
+    // transitionType: TransitionType.none);
 
-    router.define('/provider', handler: _counterProviderHandler,
-      transitionType: TransitionType.fadeIn);
+    router.define('/stateful',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+
+    router.define('/provider',
+        handler: _counterProviderHandler,
+        transitionType: TransitionType.fadeIn);
+
+    router.notFoundHandler = _pageNotFound;
   }
 
   // Handlers
@@ -28,4 +32,6 @@ class Flurorouter {
 
   static Handler _counterProviderHandler =
       Handler(handlerFunc: (context, params) => CounterProviderView());
+
+  static Handler _pageNotFound = Handler(handlerFunc: (_, __) => View404());
 }
