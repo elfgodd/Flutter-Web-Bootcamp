81dd7edeb66e343434a0efb54f9cfe5fee9f8a0d
ElfGod <albertogx@icloud.com>
2022-06-06 12:44:44 -0500

    Configurando las rutas

--- 02-vertical_landing_page/lib/main.dart
+++ 02-vertical_landing_page/lib/main.dart
@@ -1,17 +1,31 @@
 // mateapp keyboard shortcut to create basic starter
 import 'package:flutter/material.dart';
 
+import 'package:vertical_landing_page/router/router.dart';
 import 'package:vertical_landing_page/ui/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: HomePage(),
+      title: 'Landing Page',
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
     );
   }
 }
