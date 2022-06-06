742ead734d5f5b8a729dcf746b4313ecf112c0da
ElfGod <albertogx@icloud.com>
2022-06-06 16:22:23 -0500

    Opciones del menú

--- 02-vertical_landing_page/lib/ui/shared/custom_app_menu.dart
+++ 02-vertical_landing_page/lib/ui/shared/custom_app_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vertical_landing_page/ui/shared/custom_menu_item.dart';
 
 class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({Key? key}) : super(key: key);
@@ -8,8 +9,8 @@ class CustomAppMenu extends StatefulWidget {
   State<CustomAppMenu> createState() => _CustomAppMenuState();
 }
 
-class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProviderStateMixin {
-
+class _CustomAppMenuState extends State<CustomAppMenu>
+    with SingleTickerProviderStateMixin {
   bool isOpen = false;
   late AnimationController? controller;
 
@@ -18,10 +19,8 @@ class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProvider
     // TODO: implement initState
     super.initState();
 
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration( milliseconds: 200)
-      );
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
   }
 
   @override
@@ -33,7 +32,7 @@ class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProvider
         //   print('click');
         // },
         onTap: () {
-          if ( isOpen ) {
+          if (isOpen) {
             controller!.reverse();
           } else {
             controller!.forward();
@@ -43,30 +42,65 @@ class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProvider
           });
         },
         child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            width: 150,
-            height: 50,
-            color: Colors.black,
-            child: Row(
-              children: [
-                AnimatedContainer(
-                  duration: Duration(milliseconds: 200),
-                  curve: Curves.easeInOut,
-                  width: isOpen ? 50 : 0,
-                ),
-                Text('Menu',
-                    style: GoogleFonts.roboto(
-                      color: Colors.white,
-                      fontSize: 18,
-                    )),
-                Spacer(),
-                // Icon(Icons.menu, color: Colors.white),
-                AnimatedIcon(
-                    icon: AnimatedIcons.menu_close,
-                    progress: controller!,
-                    color: Colors.white),
-              ],
-            )),
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          width: 150,
+          height: isOpen ? 308 : 50,
+          color: Colors.black,
+          child: Column(
+            children: [
+              _MenuTitle(isOpen: isOpen, controller: controller),
+              if ( isOpen )
+              ...[
+                CustomMenuItem(text: 'Home', onPressed: () {}),
+                CustomMenuItem(text: 'About', onPressed: () {}),
+                CustomMenuItem(text: 'Pricing', onPressed: () {}),
+                CustomMenuItem(text: 'Contact', onPressed: () {}),
+                CustomMenuItem(text: 'Location', onPressed: () {}),
+                SizedBox(height: 8),
+              ]
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MenuTitle extends StatelessWidget {
+  const _MenuTitle({
+    Key? key,
+    required this.isOpen,
+    required this.controller,
+  }) : super(key: key);
+
+  final bool isOpen;
+  final AnimationController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 50,
+      child: Row(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isOpen ? 50 : 0,
+          ),
+          Text('Menu',
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 18,
+              )),
+          Spacer(),
+          // Icon(Icons.menu, color: Colors.white),
+          AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: controller!,
+              color: Colors.white),
+        ],
       ),
     );
   }
