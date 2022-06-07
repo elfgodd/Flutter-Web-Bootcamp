e4b1b3144e96003a3d2c32d73d3d5f444d3d0175
ElfGod <albertogx@icloud.com>
2022-06-06 21:12:46 -0500

    Cambiar el titulo de la página web

--- 02-vertical_landing_page/lib/providers/page_provider.dart
+++ 02-vertical_landing_page/lib/providers/page_provider.dart
@@ -5,12 +5,14 @@ import 'package:universal_html/html.dart' as html;
 class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
 
-  List<String> _pages = ['home', 'about', 'pricing', 'location', 'contact'];
+  List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
   int _currentIndex = 0;
 
   createScrollController(String routeName) {
     this.scrollController =
         new PageController(initialPage: getPageIndex(routeName));
+
+    html.document.title = _pages[getPageIndex(routeName)];
 
     this.scrollController.addListener(() {
       final index = (this.scrollController.page ?? 0).round();
@@ -18,6 +20,7 @@ class PageProvider extends ChangeNotifier {
       if (index != _currentIndex) {
         html.window.history.pushState(null, 'none', '#/${_pages[index]}');
         _currentIndex = index;
+        html.document.title = _pages[index];
       }
 
       // print(pageIndex);
