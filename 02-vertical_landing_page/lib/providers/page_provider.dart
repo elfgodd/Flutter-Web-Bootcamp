ef83d73fdf3dfc47f6a5e41248942bb485a2bd9c
ElfGod <albertogx@icloud.com>
2022-06-06 20:41:53 -0500

    Cambiar el URL al hacer click en un enlace

--- 02-vertical_landing_page/lib/providers/page_provider.dart
+++ 02-vertical_landing_page/lib/providers/page_provider.dart
@@ -1,9 +1,18 @@
+// import 'dart:html' as html;
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
 
+  List<String> _pages = ['home', 'about', 'pricing', 'location', 'contact'];
+
   goTo( int index ) {
+
+    // final routeName = _pages[index];
+    // print(routeName);
+
+    html.window.history.pushState(null, 'none', '#/${_pages[index]}');
 
     scrollController.animateToPage( 
       index, 
